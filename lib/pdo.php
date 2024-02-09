adb9b31f899405aa2fa5473357c3bf82322aab6a
Antoine ZEBAZE <zeba_antoine@yahoo.fr>
2024-02-09 15:22:20 +0100

    define

--- lib/pdo.php
+++ lib/pdo.php
@@ -1,6 +1,8 @@
 <?php
+
+require_once  __DIR__. '/config/config.php';
 try{
-$pdo = new PDO("mysql:dbname=garagevincentparrot;host=localhost; charser=utf8mb4", "root","");
+$pdo = new PDO("mysql:dbname="._DB_NAME_.";host="._DB_SERVER_."; charser=utf8mb4", _DB_USER_ , _DB_PASSWORD_);
 }
 catch(Exception $e){
     die('Erreur'.$e->getMessage());
