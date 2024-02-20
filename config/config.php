08f7142fef6e8bc07381238d4eba2a5b0040946c
Antoine ZEBAZE <zeba_antoine@yahoo.fr>
2024-02-20 19:31:01 +0100

    ajout variable de connexionde alwaysdata

--- config/config.php
+++ config/config.php
@@ -1,9 +1,9 @@
 <?php
 define("_WELLCOME_PAGE_", "Bienvenue sur notre page");
-define("_DB_SERVER_", "localhost");
-define("_DB_NAME_", "garage_vincent_parrot");
-define("_DB_USER_", "root");
-define("_DB_PASSWORD_", "");
+define("_DB_SERVER_", "mysql-zeba.alwaysdata.net");
+define("_DB_NAME_", "zeba_parrotvincent");
+define("_DB_USER_", "zeba");
+define("_DB_PASSWORD_", "Zebson79");
 define("_ASSETS_IMAGES_DEFAULT_", "/assets/images/");
 define("_UPLOADS_IMAGES_", "/uploads/vehicules/");
 define("_HOME_VEHICULES_LIMIT_", 4);
--- contact.php
+++ contact.php
@@ -4,10 +4,22 @@ require_once  __DIR__. '/lib/menu.php';
 require_once  __DIR__. '/templates/header.php';
 ?>
 
-<h1> <?=_WELLCOME_PAGE_?> contact !</h1>
-<h1 class="mx-5"> LogIn</h1>
+<h1 class="text-center"> <?=_WELLCOME_PAGE_?> contact !</h1>
+<h2 class="mx-5"> Remplir le formulaire ci-dessous</h2>
+<form action="#" method="post">
+    <div class="mb-3 mx-5">
+        <label for="last_name" class="form-label">Nom</label>
+        <input type="text" name="last_name" id="last_name" class="form-control">
+    </div>
+    <div class="mb-3 mx-5">
+        <label for="message" class="form-label">Votre Message</label>
+        <textarea type="textarea" name="message" id="message" class="form-control"></textarea>
+    </div>
+    
+    <input type="submit" value="Envoyer" name="messageContact" class="btn btn-primary mx-5">
+    <a href="register.php " class="nav-item btn btn-primary mx-3 ">inscrivez-vous</a>
+</form>
 
-<a href="register.php " class="nav-item btn btn-primary mx-3 ">S'inscrire</a>
 <?php
 require_once  __DIR__. '/templates/footer.php';
 ?>
